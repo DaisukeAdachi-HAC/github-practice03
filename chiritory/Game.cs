d0db376539bcffce03d3de9125bf88cadb0683d5
Daisuke Adachi <g023g1014@g.neec.ac.jp>
2024-01-25 13:42:01 +0900

    Imageクラスの作成

--- chiritory/Game.cs
+++ chiritory/Game.cs
@@ -3,10 +3,12 @@ using MyLib; // MyRandomとかInputとか使うのに必要
 
 namespace chiritory
 {
+
     public class Game
     {
         public void Init()
         {
+            Image.Load(); // 画像の読み込み
             MyRandom.Init(); // MyRandomの初期化
             Input.Init(); // Inputの初期化
         }
@@ -18,6 +20,7 @@ namespace chiritory
 
         public void Draw()
         {
+            DX.DrawGraph(0, 0, Image.woodFloor); // 背景描画
         }
     }
 }
