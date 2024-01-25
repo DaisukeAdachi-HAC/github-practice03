3dad9c84144c409413871f821a59a787d156c080
Daisuke Adachi <g023g1014@g.neec.ac.jp>
2024-01-25 12:28:15 +0900

    便利クラスの追加

--- chiritory/Game.cs
+++ chiritory/Game.cs
@@ -1,4 +1,5 @@
 ﻿using DxLibDLL;
+using MyLib; // MyRandomとかInputとか使うのに必要
 
 namespace chiritory
 {
@@ -6,10 +7,13 @@ namespace chiritory
     {
         public void Init()
         {
+            MyRandom.Init(); // MyRandomの初期化
+            Input.Init(); // Inputの初期化
         }
 
         public void Update()
         {
+            Input.Update(); // Inputの更新
         }
 
         public void Draw()
