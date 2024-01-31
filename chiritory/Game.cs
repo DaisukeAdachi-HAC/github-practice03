ee9f609055220c943daa161a76f552b1d04c35d5
Daisuke Adachi <g023g1014@g.neec.ac.jp>
2024-01-31 18:40:16 +0900

    スコアを追加する

--- chiritory/Game.cs
+++ chiritory/Game.cs
@@ -8,6 +8,7 @@ namespace Chiritori
     {
         Chiritori[] chiritories;
         Gomi gomi; // ゴミ
+        int[] scores; // 2人分のスコアを入れるための配列
 
         public void Init()
         {
@@ -17,25 +18,28 @@ namespace Chiritori
             chiritories = new Chiritori[2]; // チリトリーの配列を生成
             chiritories[0] = new Chiritori(DX.PAD_INPUT_1, Image.chiritoriGreen); // Player1生成
             chiritories[1] = new Chiritori(DX.PAD_INPUT_2, Image.chiritoriRed); // Player2生成
+            scores = new int[2]; // 配列の生成
             gomi = new Gomi(); // ゴミの生成
         }
 
         public void Update()
         {
             Input.Update(); // Inputの更新
+
             for (int i = 0; i < chiritories.Length; i++)
             {
                 Chiritori chiritori = chiritories[i];
+
                 chiritori.Update(); // チリトリーの更新
 
                 // チリトリーとゴミの距離を調べる
                 float deltaX = chiritori.x - gomi.x; // x方向の差分
                 float deltaY = chiritori.y - gomi.y; // y方向の差分
                 float distance = (float)Math.Sqrt(deltaX * deltaX + deltaY * deltaY); // 距離
-
-                // 距離が50以下なら
+                                                                                      // 距離が50以下なら
                 if (distance <= 50)
                 {
+                    scores[i] += 1; // 吸ったプレイヤーのスコアを加算
                     gomi.ResetPosition(); // ゴミの場所をリセット
                 }
             }
@@ -49,7 +53,12 @@ namespace Chiritori
             {
                 chiritories[i].Draw();
             }
+
+            // スコアを描画
+            for (int i = 0; i < scores.Length; i++)
+            {
+                DX.DrawString(300 + 60 * i, 20, scores[i].ToString(), DX.GetColor(255, 255, 255));
+            }
         }
     }
 }
-
